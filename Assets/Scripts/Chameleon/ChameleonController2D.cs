7d1b041ab8dffae729c65ec3b0499bbc9c3d76ca
JoeriVancauwenbergh <97045619+JoeriVancauwenbergh@users.noreply.github.com>
2023-05-23 19:29:21 +0200

    chameleon changes

--- Assets/Scripts/Chameleon/ChameleonController2D.cs
+++ Assets/Scripts/Chameleon/ChameleonController2D.cs
@@ -3,19 +3,21 @@ using UnityEngine;
 public class ChameleonController2D : BaseController2D
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private GameObject _base;
+    [SerializeField] private GameObject _camoBrown;
+    [SerializeField] private GameObject _camoGreen;
 
     public bool isBaseAnimation;
     public bool isBrownAnimation;
     public bool isGreenAnimation;
 
     public float animationSpeed;
-
-
     protected override void Update()
     {
-        _animator.SetFloat("ChameleonBaseSpeed", Mathf.Abs(Movement.x));
-
-
+        _animator.SetFloat("Speed", Mathf.Abs(Movement.x));
+        _animator.SetBool("IsBase", _base.activeSelf);
+        _animator.SetBool("IsBrown", _camoBrown.activeSelf);
+        _animator.SetBool("IsGreen", _camoGreen.activeSelf);
 
         base.Update();
     }
