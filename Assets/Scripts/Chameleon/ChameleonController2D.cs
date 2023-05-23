cf93a431a1fa6fe1a962594ea6093066de348cb3
RicoCriel <rico.criel@hotmail.be>
2023-05-23 19:12:59 +0200

    Basic bools

--- Assets/Scripts/Chameleon/ChameleonController2D.cs
+++ Assets/Scripts/Chameleon/ChameleonController2D.cs
@@ -4,9 +4,18 @@ public class ChameleonController2D : BaseController2D
 {
     [SerializeField] private Animator _animator;
 
+    public bool isBaseAnimation;
+    public bool isBrownAnimation;
+    public bool isGreenAnimation;
+
+    public float animationSpeed;
+
+
     protected override void Update()
     {
         _animator.SetFloat("ChameleonBaseSpeed", Mathf.Abs(Movement.x));
+
+
 
         base.Update();
     }
