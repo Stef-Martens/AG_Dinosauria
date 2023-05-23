1194f01319b6ed7ba847c13baa64c333d6417683
RicoCriel <rico.criel@hotmail.be>
2023-05-23 19:09:54 +0200

    Setup chameleon animator controller
    Transitions

--- Assets/Scripts/Chameleon/ChameleonController2D.cs
+++ Assets/Scripts/Chameleon/ChameleonController2D.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class ChameleonController2D : BaseController2D
 {
-    [SerializeField] private Animator _baseAnimator;
-    [SerializeField] private Animator _brownAnimator;
-    [SerializeField] private Animator _greenAnimator;
+    [SerializeField] private Animator _animator;
 
     protected override void Update()
     {
-        _baseAnimator.SetFloat("ChameleonBaseSpeed", Mathf.Abs(Movement.x));
-        _brownAnimator.SetFloat("ChameleonBrownSpeed", Mathf.Abs(Movement.x));
-        _greenAnimator.SetFloat("ChameleonGreenSpeed", Mathf.Abs(Movement.x));
+        _animator.SetFloat("ChameleonBaseSpeed", Mathf.Abs(Movement.x));
 
         base.Update();
     }
