a1962acc953a885f3323e00702fb7a5fc5f64475
JoeriVancauwenbergh <97045619+JoeriVancauwenbergh@users.noreply.github.com>
2023-05-31 04:04:39 +0200

    Implemented Endscreens

--- Assets/Scripts/Quiz/SwitchToNextQuiz.cs
+++ Assets/Scripts/Quiz/SwitchToNextQuiz.cs
@@ -6,10 +6,46 @@ public class SwitchToNextQuiz : MonoBehaviour
 {
     public int AnimalIndex;
     public List<GameObject> Quizzes = new List<GameObject>();
-    
+
+    [Space(10)]
+    public GameObject Animal;
+    public GameObject EndScreen;
+
     private int _currentQuizIndex = 0;
     private Text _questionText;
     private List<string> _questions = new List<string>();
+
+    private Inputs _inputs;
+    private bool _canEnd = false;
+    private bool _isEndScreenKeyPressed = false;
+
+    private void OnEnable()
+    {
+        _inputs = FindObjectOfType<Inputs>();
+
+        _inputs.ActionInputEvent += OnActionConfirmInput;
+       _inputs.ConfirmInputEvent += OnActionConfirmInput;
+    }
+
+    private void OnDisable()
+    {
+        _inputs.ActionInputEvent -= OnActionConfirmInput;
+        _inputs.ConfirmInputEvent -= OnActionConfirmInput;
+    }
+
+    private void OnActionConfirmInput(bool newActionConfirmState)
+    {
+        if (newActionConfirmState && EndScreen.gameObject.activeSelf)
+        {
+            if (!_canEnd)
+                _canEnd = true;
+            else
+            {
+                StartCoroutine(FindObjectOfType<FirebaseManager>().UpdateUSer(AnimalIndex));
+                FindObjectOfType<SceneSwitch>().ChangeScene("Selectionscreen");
+            }
+        }
+    }
 
     void Start()
     {
@@ -25,6 +61,9 @@ public class SwitchToNextQuiz : MonoBehaviour
 
             Quizzes[0].transform.gameObject.SetActive(true);
             _questionText.text = _questions[0];
+            this.transform.GetChild(0).gameObject.SetActive(true);
+            Animal.SetActive(true);
+            EndScreen.gameObject.SetActive(false); ;
         }
     }
 
@@ -49,8 +88,18 @@ public class SwitchToNextQuiz : MonoBehaviour
         else
         {
             _questionText.text = "";
-            StartCoroutine(FindObjectOfType<FirebaseManager>().UpdateUSer(AnimalIndex));
-            FindObjectOfType<SceneSwitch>().ChangeScene("Selectionscreen");
+            _isEndScreenKeyPressed = true;
+            ShowEndScreen(); 
+        }
+    }
+
+    private void ShowEndScreen()
+    {
+        if (!EndScreen.gameObject.activeSelf && _isEndScreenKeyPressed)
+        {
+            this.transform.GetChild(0).gameObject.SetActive(false);
+            Animal.SetActive(false);
+            EndScreen.gameObject.SetActive(true);
         }
     }
 }
